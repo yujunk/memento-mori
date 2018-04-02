9783512cbc5764cc61f926f19e8ecf870e09b455
yujunk <yujunk@gmail.com>
2018-04-03 00:08:58 +0800

    dashboard created

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get "/sign_up" => "users#new", as: "sign_up"
   get "/sign_in" => "sessions#new", as: "sign_in"
   delete "/sign_out" => "sessions#destroy", as: "sign_out"
+  get "/settings" => "sessions#settings", as: "settings"
+
+  get "/:user_id/dashboard" => "dashboard#show", as: "dashboard"
 end
