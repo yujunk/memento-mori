7a22c45eefc97f3fe17e677d6e3f51c9dcf0929f
yujunk <yujunk@gmail.com>
2018-04-02 20:04:38 +0800

    log outs included with nav bar on application.html. Though bootstrap is not working.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  #before_action :authenticate_user, :only => [:home, :profile, :setting]
+  before_action :authenticate_user, :only => [:profile, :setting]
   before_action :save_login_state, :only => [:new, :create]
 
   def new
@@ -11,13 +11,25 @@ class SessionsController < ApplicationController
 
     if @authorized_user
       session[:user_id] = @authorized_user.id
+      flash[:notice] = "Welcome, #{@authorized_user.first_name}."
       redirect_to root_url
     else
       @user = User.new
+      flash[:notice] = "Invalid username or password."
       redirect_to sign_in_path
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url
+  end
+
+  def profile
+  end
+
+  def setting
+  end
 end
 
 #https://www.sitepoint.com/rails-userpassword-authentication-from-scratch-part-ii/
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :save_login_state, :only => [:new, :create]
+  
   def new
     @user = User.new
   end
@@ -7,6 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save
+
+    session[:user_id] = @user.id
 
     redirect_to "/sign_in"
   end
